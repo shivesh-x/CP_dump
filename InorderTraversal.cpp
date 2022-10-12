b21e8557b290c385eb6b32ec71c71834e30e2e89
StutiSharma27 <92028077+StutiSharma27@users.noreply.github.com>
2022-10-12 21:26:26 +0530

    Create InorderTraversal.cpp
    The following code displays inorder traversal in a binary tree

---
+++ InorderTraversal.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+using namespace std;
+
+struct node
+{
+    int data;
+    node *lc;
+    node *rc;
+};
+
+struct que 
+{
+    int status;
+    node *ptr;
+    que *next;
+};
+struct que *first=NULL;
+struct que *last=NULL;
+
+node *insert(int x,node *root)
+{
+    node *temp=new node;
+    que *q=new que;
+    temp->data=x;
+    temp->lc=NULL;
+    temp->rc=NULL;
+    q->status=0;
+    q->next=NULL;
+    
+    if(root==NULL)
+    {
+        q->ptr=temp;
+        first=last=q;
+        return temp;
+    }
+    else
+    {
+        if(first->status==0)
+        {
+            first->ptr->lc=temp;
+            first->status=1;
+        }
+        else
+        {
+            first->ptr->rc=temp;
+            first=first->next;
+        }
+        last->next = q;
+        q->ptr = temp;
+        last = q;
+    }
+    return root;
+}
+void inorder(node *root)
+{
+    if(root == NULL)
+    return;
+    else
+    {
+        cout<<root->data<<" ";
+        inorder(root->lc);
+        inorder(root->rc);
+    }
+}
+
+int main()
+{
+    node *root = NULL;
+    cout<<"Tree Elements in Array Form : ";
+    for(int i = 0; i < 10; i++)
+    {
+        cout<<i<<" ";
+        root = insert(i,root);
+    }
+    cout<<"\nInorder Traversal : ";
+    inorder(root);
+    return 0;
+}
